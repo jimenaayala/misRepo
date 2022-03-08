e864f09261b4b50a5e9e186d277bee2bef62c860
jimenaayala <jayala@silice.biz>
2022-03-08 11:29:38 -0300

    segundo commit

---
+++ src/Entidades/ejemploGit.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Jime
+ */
+public class ejemploGit {
+    private int uno;
+
+    public ejemploGit() {
+    }
+    
+}
